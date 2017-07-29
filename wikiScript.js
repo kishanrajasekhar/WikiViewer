6bcc3cef078ae9c3d8d34fb22f2ec4b4d13ec2c2
kishanrajasekhar <krajasek@uci.edu>
2017-07-29 12:47:38 -0400

    added comments and random article button

--- wikiScript.js
+++ wikiScript.js
@@ -1,10 +1,19 @@
 // api information from https://www.mediawiki.org/wiki/API:Search
 
+// base_url is used to access the api date (in json format). A query
+// needs to be appended to it, which is done in the 
+// displaySearchResults function
 var base_url = "https://en.wikipedia.org/w/api.php?action=query&list=search&&utf8=&format=json&callback=?&srsearch=";
-//var base_redirect = "https://en.wikipedia.org/?";
+
+// base_redirect is the regular url to the wikipedia page. After 
+// appending the title of the wikipedia article, clicking this
+// link will direct the user to that wikipedia page.
 var base_redirect = "https://en.wikipedia.org/wiki/";
 
-//
+// This function is called when the user presses
+// the "Search" button after typing in the query.
+// The query is retrieved from the search box and
+// passed in the displaySearchResults function.
 function searchQuery(){
 	// clear the previous search results
 	$("#content").html("");
@@ -15,6 +24,11 @@ function searchQuery(){
 	$("#searchBox").val("");
 }
 
+// Displays the wiki search results from the 
+// user's query by parsing the json from the
+// wikipedia API. Any old search results will
+// be replaced by the new results. Each result
+// has a hyperlinked to the actual wikipedia page.
 function displaySearchResults(query){
 	// create the complete url
 	var wiki_url = base_url + query;
@@ -29,29 +43,4 @@ function displaySearchResults(query){
 		}
 	});
 	
-}
-
-function test(){	
-	query = "Albert%20Einstein";
-	
-	//var test_url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Albert%20Einstein&utf8=&format=json&callback=?";	
-	var test_url = base_url + query;
-	
-	$.getJSON(test_url, function(json){
-		var result = json["query"]["search"];
-		for(var i=0; i<result.length; i++){
-			$("#content").append("<h3>" + result[i].title + "</h3><br>");
-			$("#content").append(result[i].snippet  + "<br>");
-		}
-		
-		/*
-		jQuery.each(content, function (){
-			var result_text = this["revisions"][0]["*"];
-			$("#content").html(result_text);
-		});
-		$("#content").html(content);
-		var newDoc = document.open("text/html", "replace");
-		newDoc.write(content);
-		newDoc.close();*/
-	});
 }
